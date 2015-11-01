2a627eef43c029d1eb89f2d3de8d426ac677b854
Sachin Kumar <kumar.sachin52@gmail.com>
2015-11-01 12:27:51 +0530

    Vignere encrypt decrypt done

---
+++ vignere.py
@@ -0,0 +1,64 @@
+class Vignere:
+	'''
+	This class describes encryption and decryption functions of Vignere Cipher
+	'''
+	@staticmethod
+	def encrypt(
+				plaintext, 
+				key, 
+				m
+				):
+		'''
+		encrypts plain text
+
+		parameters
+		------------------------------------
+	   	string: plaintext(lower case)
+	   	string: key(upper case)
+	   	int: m
+
+	   	returns
+	   	------------------------------------
+	    string: ciphertext
+	    '''
+
+		ciphertext = ""
+		for i,x in enumerate(plaintext):
+			y = (ord(x) - ord('a') + ord(key[i%m]) - ord('A')) % 26
+			y = y + ord('A')
+			y = chr(y)
+			ciphertext+=y
+
+		return ciphertext
+
+	@staticmethod
+	def decrypt(
+				ciphertext,
+				key,
+				m
+				):
+		'''
+		decrypts cipher text
+		parameters
+		-----------------------------------
+		string: ciphertext
+		string: key
+		int: m
+
+		returns
+		-----------------------------------
+		string: plaintext
+		'''
+		plaintext = ''
+		for i in range(len(ciphertext)):
+			plaintext += chr((ord(ciphertext[i])-ord(key[i%m])+26)%26+97)
+
+		return plaintext
+
+if __name__ == '__main__':
+	p = raw_input()
+	k = raw_input()
+
+	c = Vignere.encrypt(p, k, len(k))
+	print c
+	print Vignere.decrypt(c, k, len(k))
